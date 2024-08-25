f4b5747c6546a73f8f0af3ef8636ac58cc366b8a
Azumi <36726702+azumi-y-unicom@users.noreply.github.com>
2024-08-26 00:03:24 +0900

    fix: Read Replica yuml

--- postgres/read_replica/entrypoint.sh
+++ postgres/read_replica/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -xeu
 
-pg_basebackup \\
-	-h postgres \\
-	-D ${PGDATA} \\
-	-S my_replication_slot \\
-	-X stream \\
-	-U replicator \\
-	-R || true
+pg_basebackup -h postgres -D ${PGDATA} -S my_replication_slot -X stream -U replicator -R || true
 
 /usr/local/bin/docker-entrypoint.sh postgres
